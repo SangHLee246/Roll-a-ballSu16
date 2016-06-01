3c3bfa464e44811a7f27357018470aee7e39eac8
SangHLee246 <akayasha1234@yahoo.com>
2016-06-01 18:18:34 -0400

    Finished Video 6 + 7
    Completed all of the parts including the GUIText and the Website
    portion.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,16 @@ using System.Collections;
 public class PlayerController : MonoBehaviour 
 {
 	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+
+	void Start()
+	{
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
 
 	void FixedUpdate()
 	{
@@ -15,11 +25,23 @@ public class PlayerController : MonoBehaviour
 		rigidbody.AddForce (movement * speed * Time.deltaTime);
 	}
 
-		void OnTriggerEnter(Collider other)
+	void OnTriggerEnter(Collider other)
 	{
 		if(other.gameObject.tag == "PickUp")
 		{
 			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText ();
 		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) 
+		{
+			winText.text = "YOU WIN!";
+		}
+
 	}
 }
